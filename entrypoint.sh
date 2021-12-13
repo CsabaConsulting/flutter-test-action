3119b8db294445c47b5e143cfa6bd75170fa12e1
Csaba Toth <csaba.toth.us@outlook.com>
2021-12-13 00:37:54 -0800

    Translate and modify, disable ios build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/sh -l
 
-echo "Avvio l'installazione di Flutter nella VM."
+echo "Start installing Flutter on the VM."
 git clone https://github.com/flutter/flutter.git
 export PATH="$PATH:`pwd`/flutter/bin"
 flutter precache
 flutter channel $1
-echo "Ho installato Flutter $1 nella VM."
+echo "Installed Flutter $1 on the VM."
 
 lsresult=$(tree)
 echo "::set-output name=ls::$lsresult"
 
-flutter build apk
-flutter build ios
+flutter pub get
+flutter test
+flutter build appbundle
+#flutter build ios
